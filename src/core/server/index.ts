6354044156f70a71ef3cc261fe94bceca5dc0fc2
Carlos Menezes <carlos-menezes212@hotmail.com>
2024-02-28 22:25:34 +0000

    Update index.ts

--- src/core/server/index.ts
+++ src/core/server/index.ts
@@ -1,6 +1,1 @@
-import * as alt from '@altv/server';
-
-alt.Events.onPlayerConnect(({ player }) => {
-    player.model = 'mp_m_freemode_01';
-    player.spawn({ x: 36.19486618041992, y: 859.3850708007812, z: 197.71343994140625 }, 0);
-});
+// .gitkeep
